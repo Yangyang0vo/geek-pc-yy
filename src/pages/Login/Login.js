7869b922bdb66a50e8ae05d994bc8e153b649aba
洋洋 <2372542263@qq.com>
2022-08-16 19:39:44 +0800

    登录功能-基本结构

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
-import { Button } from 'antd'
+import { Card } from 'antd'
+import './index.css'
+// react 中不能直接导入本地的图片 不会被webpack去进行解析
+// 需要导入一个动态的
+import logo from '../../assets/logo.png'
 export default class Login extends Component {
   render() {
     return (
-      <div>
-        <div>登录组件</div>
-        <Button type="primary">哈哈哈</Button>
+      <div className="login">
+        <Card className="login-container">
+          <img src={logo} alt="" className="login-logo" />
+        </Card>
       </div>
     )
   }
