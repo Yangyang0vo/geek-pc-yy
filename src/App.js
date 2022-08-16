0d0fb127878a8409154d5e26b706ad44ff835f34
洋洋 <2372542263@qq.com>
2022-08-16 17:39:02 +0800

    项目搭建-配置路由

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 // @ts-nocheck
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login/Login'
+import Layout from './pages/Layout/Layout'
 function App() {
-  return <div className="App">App根组件</div>
+  return (
+    <Router>
+      <div className="App">App根组件</div>
+
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/layout" element={<Layout />}></Route>
+        <Route path="*" element={<Navigate to={'/login'} />}></Route>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
